e68d80b3cc6533519ec3141ecacfeebc7cfb5463
Dennis0826-lin <jkl7860@gmail.com>
2026-08-31 14:28:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ if not api_key:
     )
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.5-flash",
     google_api_key=api_key,
 )
 llm_with_tools = llm.bind_tools(tools)
