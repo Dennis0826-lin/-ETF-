dfe2498f15199f7ad57f25685bc0537781ee1c9d
Dennis0826-lin <jkl7860@gmail.com>
2026-09-07 10:46:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,11 @@ from google.oauth2.service_account import Credentials
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.tools import tool
-from langchain_google_genai import (
-    ChatGoogleGenerativeAI,
-    GoogleGenerativeAIEmbeddings,
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from google.genai.types import (
+    Tool as GoogleTool,
+    GoogleSearch,
 )
 
 from langgraph.graph import END, START, StateGraph
